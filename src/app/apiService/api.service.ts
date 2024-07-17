9ee389c1b0aa8e9ba0fbeb3043dd4930d8d3f142
Prathap <prathap.p@oasyscybernetics.in>
2024-07-17 16:27:43 +0530

    builded2

--- src/app/apiService/api.service.ts
+++ src/app/apiService/api.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs/internal/Observable';
 
 @Injectable({
@@ -7,7 +8,7 @@ import { Observable } from 'rxjs/internal/Observable';
 })
 export class ApiService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient,private router:Router) { }
 
   private url='https://datacode.onrender.com/';
 
@@ -19,5 +20,10 @@ export class ApiService {
   postData(path:string,data:any){
     return this.http.post(this.url+path,data);
 
+  }
+
+  logout(){
+    localStorage.removeItem("user");
+    this.router.navigate(["/"]);
   }
 }
